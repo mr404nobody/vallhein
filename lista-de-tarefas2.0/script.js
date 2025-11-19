bb025c97dab09af04c26269fc7f93fb8cf104d02
mr404nobody <mr404nobody@gmail.com>
2025-11-18 21:01:18 -0300

    darkmde

--- lista-de-tarefas2.0/script.js
+++ lista-de-tarefas2.0/script.js
@@ -3,9 +3,14 @@ const ul = document.querySelector("ul")
 const texto = document.querySelector("#texto")
 const aviso = document.getElementById("aviso")
 
+const tema = document.getElementById("tema")
+const listTarefas = document.querySelector(".lista-tarefas")
+const container = document.querySelector(".container")
+
 let avs = false;
 
 add.addEventListener("click", addTarefa)
+tema.addEventListener("click", mudarTema)
 
 function addTarefa() {
     if(texto.value !== "") {
@@ -43,5 +48,19 @@ function addTarefa() {
     } else if(avs === false) {
         aviso.style.display = "inline-block";
         avs = true;
+    }
+}
+
+let temaEscuro = false;
+
+function mudarTema() {
+    listTarefas.classList.toggle("tema-escuro")
+    container.classList.toggle("fundo-escuro")
+    if(temaEscuro === false) {
+        tema.textContent = "Tema: Escuro";
+        temaEscuro = true;
+    } else if(temaEscuro !== false) {
+        tema.textContent = "Tema: Claro"
+        temaEscuro = false;
     }
 }
